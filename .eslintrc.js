ee71c849358e87f2965fd082777099faf7cadad7
Tatiana Tkachenko <tkachenko.tatiana16@gmail.com>
2018-11-29 09:08:32 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+    },
+    "rules": {
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": "off"
+    }
+};
---
+++ src/tasks/2_ajax.js
@@ -0,0 +1,77 @@
+const URL = "https://my-json-server.typicode.com/tkachenko-tatiana/shop-api";
+
+// task 1
+function getProducts() {
+
+}
+
+getProducts();
+
+// task 2
+function getProductById(id) {
+
+}
+
+getProductById();
+
+
+// task 3
+function createProduct (product) {
+
+}
+
+createProduct();
+
+
+// task 4
+function updateProduct (product) {
+
+}
+
+updateProduct();
+
+// task 5
+function deleteProduct (id) {
+
+}
+
+deleteProduct();
+
+// task 6
+// Вывести 5 товаров, спросить у пользователя "Показать больше?"
+// Если да - показать еще 5 товаров, если нет выйти и т.д.
+function getProductsList() {
+
+}
+
+function showProducts () {
+
+}
+
+showProducts();
+
+
+//task 7
+// Вывести все товары, найменование которых равно аргументу функции
+function getProductByName(name) {
+
+}
+
+getProductByName();
+
+// task 8
+// Вывести все товары, цена которых больше или равна аргумента функции
+function getProductsByPrice(price) {
+
+}
+
+getProductsByPrice();
+
+// task 9
+// Вывести все товары в порядке убывания/возрастания цены в зависимости от аргумента функции
+function sortByPrice(order) {
+
+}
+
+sortByPrice();
+
